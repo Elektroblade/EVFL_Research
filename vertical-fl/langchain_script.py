d32bb0348bfbe842273e31c59cdeda6f4dfc64f5
Elektroblade <106491949+Elektroblade@users.noreply.github.com>
2026-02-25 14:47:59 -0800

    remote llama3.1 8b usage

--- vertical-fl/langchain_script.py
+++ vertical-fl/langchain_script.py
@@ -8,45 +8,23 @@ from langchain_core.messages import SystemMessage, HumanMessage
 
 
 def main():
-    model_id = "meta-llama/Llama-3.1-8B"
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.float16,
-    )
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_id,
-        quantization_config=bnb_config,
-        dtype=torch.float16,
-        device_map="auto"
-    )
-
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_new_tokens=512,
+    llm = HuggingFaceEndpoint(
+        repo_id="meta-llama/Llama-3.1-8B-Instruct",
+        huggingfacehub_api_token=os.environ["HUGGINGFACEHUB_API_TOKEN"],
+        task="text-generation",
         temperature=0.1,
-        top_p=0.9,
-        do_sample=False,
+        max_new_tokens=256,
     )
 
-    chat = ChatHuggingFace(pipeline=pipe)
+    chat = ChatHuggingFace(llm=llm)
 
-    #response = chat("Explains LLMs in one sentence.")
-    #print(response)
-
-    
     messages = [
         SystemMessage(content="You are a helpful assistant."),
         HumanMessage(content="Explain LLMs in one sentence.")
     ]
-    chat(messages)
+
+    response = chat.invoke(messages)
+    print(response.content)
 
 
 if __name__ == "__main__":
